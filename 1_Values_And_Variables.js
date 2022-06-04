f1fbb1817f7e43296e1cec241e9e5c89c120d707
ZahidChand <87262433+ZahidChand@users.noreply.github.com>
2022-06-04 13:11:27 +0530

    Values And Variables

---
+++ 1_Values_And_Variables.js
@@ -0,0 +1,24 @@
+/*** Values And Variables In Javascript ***/
+
+/*
+    variables are Use to store data or values
+    Naming conventions are very important while making a variables.
+    You cannot simply write anyname as variables.
+    Naming should be relatable to what data or values its holding.
+
+    -- The first charecter must be a letter or a underscore (_) or a dollar($) sign. You cannot use a number as a 
+    first charecter .
+
+    -- The rest of the variable name can include any letter , any number, or the underscore. 
+    Can't use any other charecters , including space.
+
+    -- variables names are case sensitive.
+
+    -- No limt to the length of variable name
+
+    -- You can't use any javascript reserved words as a variables.
+
+*/
+var myName = 'Zahid Chand';
+console.log(myName);
+

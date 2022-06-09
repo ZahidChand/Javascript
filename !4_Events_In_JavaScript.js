5d58c043eec0c2b58ce635c3a7211fdaff777922
Zahid Chand <87262433+ZahidChand@users.noreply.github.com>
2022-06-09 12:56:56 +0530

    Events

---
+++ !4_Events_In_JavaScript.js
@@ -0,0 +1,58 @@
+/**
+ *      Events In Javascript
+ *      
+ *  HTML events are "things" that happen to HTML elements.
+
+    When JavaScript is used in HTML pages, JavaScript can "react" on these events.
+
+    An HTML event can be something the browser does, or something a user does.
+
+
+    Here are some examples of HTML events:
+
+        An HTML web page has finished loading
+        An HTML input field was changed
+        An HTML button was clicked
+
+
+    Often, when events happen, you may want to do something.
+
+    JavaScript lets you execute code when events are detected.
+
+    HTML allows event handler attributes, with JavaScript code, to be added to HTML elements.
+
+     4 ways of writing Events in JavaScript
+
+ 1: using inline events alert();
+ 2: By Calling a funcion (We already seen and most common way of writing) 
+ 3: using Inline events (HTML onclick="" property and element.onclick)
+ 4: using Event Listeners (addEventListener and IE's attachEvent)
+
+
+ Q . What is Event Object?
+        Event object is the parent object of the event object. 
+        for Example 
+        MouseEvent, focusEvent, KeyboardEvent etc 
+
+
+ Q . MouseEvent in JavaScript
+        The MouseEvent Object
+        Events that occur when the mouse interacts with the HTML 
+        document belongs to the MouseEvent Object.
+
+
+
+ Q . KeyboardEvent  in JavaScript
+        Events that occur when user presses a key on the keyboard, 
+        belongs to the KeyboardEvent Object.
+ 
+
+
+ Q .InputEvents in JavaScript 
+        The onchange event occurs when the value of an element has been changed.
+        For radiobuttons and checkboxes, the onchange event occurs when the 
+        checked state has been changed.
+
+
+
+ */ 
